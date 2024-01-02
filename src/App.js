08b1a4c0d01347c7a23e63e11a90078c36ef4a77
RandallSungcang <122940152+RandallSungcang@users.noreply.github.com>
2024-01-02 15:36:58 -0600

    Update home page
    Updated home page top bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Landing } from './Landing';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Router>
         <div className="top-bar">
           <div className="top-Icon">
-        <img src={logo} className="App-logo" alt="logo" />        </div>
+            <Link exact to="/"><button className='home-Icon'><img src={logo} className="App-logo" alt="logo" /></button> </Link>
+          </div>
+          <div className='top-middle'>
+            <h3>Ron Website</h3>
+          </div>
+          <div className="top-right">
+            <button className='top-buttons'>Contact</button>
+          </div>
+        </div>
         
-        <div className="top-buttons">
-        <button>Book</button>
-        <button>Consult</button>
-        <button>Contact</button>
-        </div>
-        </div>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+        </Routes>
+        </Router>
         </header>
     </div>
   );
---
+++ src/Landing.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export const Landing = () => {
+
+    return (
+        <>
+        <button>Book</button>
+        <button>Consult</button>
+        </>
+    );
+}
