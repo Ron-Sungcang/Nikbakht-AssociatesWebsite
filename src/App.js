d3538fa3fdf3af586996befc0bb4ebdfcbab34f3
RandallSungcang <122940152+RandallSungcang@users.noreply.github.com>
2024-01-02 12:12:57 -0600

    Buttons and app design
    Added the three necessary buttons and UI showcase

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="top-bar">
+          <div className="top-Icon">
+        <img src={logo} className="App-logo" alt="logo" />        </div>
+        
+        <div className="top-buttons">
+        <button>Book</button>
+        <button>Consult</button>
+        <button>Contact</button>
+        </div>
+        </div>
+        </header>
     </div>
   );
 }
